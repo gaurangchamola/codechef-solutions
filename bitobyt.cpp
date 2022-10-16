156c618f9196e1c37740a6a28d7b87f93ccf0b9b
co17359 <92684217+co17359@users.noreply.github.com>
2022-10-16 17:52:15 +0530

    Added: Solution to bittobyt

---
+++ bitobyt.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+/*
+int main()
+{
+    cout<<"sdfsdfs";
+    int t,n;
+    cin>>t;
+  //int c_bit=0,c_nibble=0,c_byte=0;
+    int bit=1,nibble=0,byte=0;
+    int q_byte,r_byte,q_nibble,r_nibble,q_bit,r_bit;
+    
+    for (int i=0; i<t; i++)
+    {
+        cin>>n;
+        q_byte=n/26;
+        r_byte=n%26;
+        byte=pow(2,q_byte);
+        
+        q_nibble=r_byte/10;
+        r_nibble=r_byte%10;
+        
+        
+        
+        
+    }
+    
+    for (int k=0; k<t; k++)
+    {
+        cout<<bit<<" "<<nibble<<" "<<byte;
+    }
+
+    return 0;
+}*/
